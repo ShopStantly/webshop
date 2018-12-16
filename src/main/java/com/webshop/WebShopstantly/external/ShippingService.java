631cee39309dc317255a3ac4aa372c53e289673c
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-16 16:21:13 +0100

    added request method to save an order

--- src/main/java/com/webshop/WebShopstantly/external/ShippingService.java
+++ src/main/java/com/webshop/WebShopstantly/external/ShippingService.java
@@ -1,4 +1,26 @@
 package com.webshop.WebShopstantly.external;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
 public class ShippingService {
+
+    public String saveOrder() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "http://localhost:8083/order/saveorder";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().toString();
+    }
+
 }
